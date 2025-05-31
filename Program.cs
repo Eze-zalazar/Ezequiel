fb9ca0d78e54129ee6af6c705d9d2ca38d811868
Eze-zalazar <urielezequielzalazar@gmail.com>
2025-05-31 20:05:46 -0300

    Primer commit del proyecto SharpDevelop

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: eze
+ * Fecha: 31/5/2025
+ * Hora: 19:52
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+
+namespace tpAlgortimos
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			Console.WriteLine("hola");
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
